9b116b4f5cb4d1d1175a154913e381a6ef5b50f5
kingdaro <kingdaro@gmail.com>
2019-08-06 04:11:38 -0500

    cors does not apply

--- api/src/index.ts
+++ api/src/index.ts
@@ -134,13 +134,6 @@ async function startServer() {
     endpoint: "/api",
     playground: "/api",
     port,
-    cors: {
-      origin: [
-        "http://localhost:3000", // client dev server
-        "http://localhost:4000", // api dev server
-      ],
-      credentials: true,
-    },
   })
 
   console.log(`Api running on http://localhost:${port}`)
