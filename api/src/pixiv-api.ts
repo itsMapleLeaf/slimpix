0fc5f07316912feb70d2a82a5ce324410ddab63d
kingdaro <kingdaro@gmail.com>
2019-08-07 20:30:34 -0500

    type login response

--- api/src/pixiv-api.ts
+++ api/src/pixiv-api.ts
@@ -18,9 +18,9 @@ import querystring from "querystring"
         px_50x50: 'https://i.pximg.net/user-profile/img/(...).png',
         px_170x170: 'https://i.pximg.net/user-profile/img/(...).png',
       },
-      id: '...', // user ID
-      name: '...', // display name
-      account: '...', // username
+      id: '...', 
+      name: '...', 
+      account: '...', 
       mail_address: '...',
       is_premium: true,
       x_restrict: 2,
@@ -31,11 +31,34 @@ import querystring from "querystring"
 }
 */
 
+export type LoginResponse = {
+  access_token: string
+  expires_in: number
+  token_type: string
+  scope: string // ???
+  refresh_token: string
+  user: {
+    profile_image_urls: {
+      px_16x16: string
+      px_50x50: string
+      px_170x170: string
+    }
+    id: string // user ID
+    name: string // display name
+    account: string // username
+    mail_address: string
+    is_premium: boolean
+    x_restrict: number
+    is_mail_authorized: boolean
+  }
+  device_token: string
+}
+
 const clientId = "MOBrBDS8blbauoSck0ZfDbtuzpyT"
 const clientSecret = "lsACyCD94FhDUtGTXi3QzcFE2uU1hqtDaKeqrdwj"
 
 export class PixivApi {
-  async login(username: string, password: string) {
+  async login(username: string, password: string): Promise<LoginResponse> {
     const requestData = {
       client_id: clientId,
       client_secret: clientSecret,
@@ -51,6 +74,6 @@ export class PixivApi {
       data: querystring.stringify(requestData),
     })
 
-    return data
+    return data.response
   }
 }
